4f16dcd5e789a75636409064b9c746580fc97b8b
Danny Nguyen <dn-l@outlook.com>
2026-05-23 23:33:59 +1000

    Sizing + changed how hover works on phone

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,13 @@ const SHOW_BUILDER = false;
 const DON_IMAGE =
   "https://www.optcgapi.com/media/static/Card_Images/DON_Card__Green_Compass_-_Starter_Deck_1_Straw_Hat_Crew_ST-01_img.jpg";
 
+
+function shouldUseHoverPreview() {
+  return (
+    typeof window !== "undefined" &&
+    window.matchMedia("(hover: hover) and (pointer: fine)").matches
+  );
+}
 function CardTile({
   card,
   hidden = false,
@@ -34,14 +41,17 @@ function CardTile({
     <div
       className={className}
       onMouseEnter={() => {
-        if (!disableHoverPreview) {
+        if (!disableHoverPreview && shouldUseHoverPreview()) {
           setHoveredCard?.(card);
         }
       }}
       onMouseLeave={() => {
-        if (!disableHoverPreview) {
+        if (!disableHoverPreview && shouldUseHoverPreview()) {
           setHoveredCard?.(null);
         }
+      }}
+      onTouchStart={() => {
+        setHoveredCard?.(null);
       }}
       onClick={() => onClick?.(card)}
     >
